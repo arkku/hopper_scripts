eebf1d1d8c8993e0f88510f2d90d7576e7bf89c2
Kimmo Kulovesi <kk@arkku.com>
2015-02-17 04:52:18 +0200

    Allow forcing 64-bit in 32-bit mode

--- Insert return int.py
+++ Insert return int.py
@@ -1,7 +1,8 @@
 # Hopper Disassembler script to insert x86/x64 instructions to return
 # an integer from the current procedure. Should handle both 64-bit and
-# 32-bit values, and automatically inserts pop ebp/rbp if there was an
-# unreplaced push at the beginning of the procedure.
+# 32-bit values. Automatically inserts function prologue if its epilogue
+# remains unchanged - to avoid inserting the prologue, run this at the
+# very beginning of the function so that the epilogue is overwritten.
 #
 # By Kimmo Kulovesi <http://arkku.com/>, 2015
 
@@ -14,26 +15,31 @@ ins = seg.getInstructionAtAddress(adr)
 arch = ins.getArchitecture()
 
 if arch in [1, 2]:
-    s = Document.ask("Integer value to return:")
+    suffix = ":" if arch == 2 else " (L suffix forces 64-bit):"
+    s = Document.ask("Integer value to return"+suffix)
     if s != None:
+        valueSize = 4
+        if s[-1] == 'L' or s[-1] == 'l':
+            valueSize = 8
+            s = s[:-1]
         i = int(s, 0)
         endProc = adr + 1
         while seg.getTypeAtAddress(endProc) == Segment.TYPE_NEXT:
             endProc += 1
-        if i == 1 or i == 0:
+        if (arch == 2 or valueSize < 8) and (i == 1 or i == 0):
             # xor eax, eax -> 0
             seg.writeByte(adr, 0x31)
             seg.writeByte(adr + 1, 0xC0)
             seg.markAsCode(adr)
             adr += 2
             if i == 1:
+                # inc eax -> 1
                 seg.writeByte(adr, 0xFF)
                 seg.writeByte(adr + 1, 0xC0)
                 seg.markAsCode(adr)
                 adr += 2
         else:
             offset = 0
-            valueSize = 4
             valueChunk = 4
             if i > 4294967295 or i < -2147483648:
                 # 64-bit value
@@ -57,11 +63,33 @@ if arch in [1, 2]:
                     i >>= 8
                 seg.markAsCode(adr)
                 adr += offset + valueChunk
-        if entry != Segment.BAD_ADDRESS and seg.readByte(entry) == 0x55:
-            # pop rbp
-            seg.writeByte(adr, 0x5D)
-            seg.markAsCode(adr)
-            adr += 1
+        if entry != Segment.BAD_ADDRESS:
+            if seg.readByte(entry) == 0x55:
+                if seg.readByte(entry + 1) == 0x48 and \
+                        seg.readByte(entry + 2) == 0x89 and \
+                        seg.readByte(entry + 3) == 0xE5:
+                       # mov rsp, rbp
+                       seg.writeByte(adr, 0x48)
+                       seg.writeByte(adr + 1, 0x89)
+                       seg.writeByte(adr + 2, 0xEC)
+                       seg.markAsCode(adr)
+                       adr += 3
+                elif seg.readByte(entry + 1) == 0x89 and \
+                        seg.readByte(entry + 2) == 0xE5:
+                       # mov esp, ebp
+                       seg.writeByte(adr, 0x89)
+                       seg.writeByte(adr + 1, 0xEC)
+                       seg.markAsCode(adr)
+                       adr += 2
+                # pop rbp/ebp
+                seg.writeByte(adr, 0x5D)
+                seg.markAsCode(adr)
+                adr += 1
+            elif seg.readByte(entry) == 0xC8:
+                # leave
+                seq.writeByte(adr, 0xC9)
+                seg.markAsCode(adr)
+                adr += 1
         seg.writeByte(adr, 0xC3)
         seg.markAsCode(adr)
         adr += 1
